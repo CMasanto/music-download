06ccfa00a7b5ba0bac68804789a57e733c71bbbb
CMasanto <coreymasanto@gmail.com>
2016-06-17 15:58:51 -0400

    Add XMLHttpRequest and installed spotify-web-api-js via node.

--- js/src/MusicData.js
+++ js/src/MusicData.js
@@ -2,17 +2,17 @@ function $(id) {
 	return document.getElementById(id);
 }
 
-var Spotify = require('spotify-web-api-js');  // Think of require as an import.
+var Spotify = require('spotify-web-api-js');
 var s = new Spotify();
 
-var spotifyApi = new SpotifyWebApi();  // Instantiate the js wrapper.
+//var spotifyApi = new SpotifyWebApi();  // Instantiate the js wrapper.
 
 // Get a list of all tracks.
 // For the track, find the correct YouTube video.
 
 function getAllTrackIdsForQuery(query) {
 	// search tracks whose name, album or artist contains query
-	trackIDs = spotifyApi.searchTracks(query)
+	trackIDs = s.searchTracks(query)
   		.then(function(data) {
     		console.log('Search by "' + query + '"', data);
   		}, function(err) {
@@ -22,3 +22,22 @@ function getAllTrackIdsForQuery(query) {
  	return trackIDs;
 }
 
+function getAllTrackIdsForQueryTest() {
+	const query1 = '';
+	const result1 = getAllTrackIdsForQuery(query1);
+	print(result1);
+	
+	const query2 = '1l234hlahdfa784rklasf134';
+	const result2 = getAllTrackIdsForQuery(query2);
+	print(result2);
+	
+	const query3 = 'love';
+	const result3 = getAllTrackIdsForQuery(query3);
+	print(result3);
+	
+	const query4 = 'love is';
+	const result4 = getAllTrackIdsForQuery(query4);
+	print(result4);
+}
+
+getAllTrackIdsForQueryTest();
--- js/test/MusicDataTest.js
+++ js/test/MusicDataTest.js
@@ -1,13 +1,1 @@
-function getAllTrackIdsForQueryTest() {
-	var query1 = '';
-	var query2 =
-	var query3 =
-	var query4 =
-	var query5 =
-	var query6 =
-	var query7 =
-	var query8 =
-	var query9 =
-	var query10 =
-
-}
+// TODO: https://www.distelli.com/docs/tutorials/test-your-nodejs-with-jasmine
