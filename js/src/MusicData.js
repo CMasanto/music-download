b3d133644ad44f7c46c2110e6cb9334f519905e8
CMasanto <coreymasanto@gmail.com>
2016-06-30 04:05:26 -0400

    Added Browserify and JQuery, and continued attempts to call Spotify API

--- js/src/MusicData.js
+++ js/src/MusicData.js
@@ -1,43 +1,41 @@
-function $(id) {
-	return document.getElementById(id);
-}
-
-var Spotify = require('spotify-web-api-js');
-var s = new Spotify();
-
-//var spotifyApi = new SpotifyWebApi();  // Instantiate the js wrapper.
-
-// Get a list of all tracks.
-// For the track, find the correct YouTube video.
-
-function getAllTrackIdsForQuery(query) {
+function getAllElvisAlbums() {
 	// search tracks whose name, album or artist contains query
-	trackIDs = s.searchTracks(query)
-  		.then(function(data) {
-    		console.log('Search by "' + query + '"', data);
-  		}, function(err) {
-			console.error(err);
- 		});
- 	
- 	return trackIDs;
+	var unique = require('uniq');
+	var Spotify = require('spotify-web-api-js');
+	var s = new Spotify();
+	
+	// First, instantiate the wrapper.
+	var spotifyApi = new SpotifyWebApi();
+	
+	// get Elvis' albums, passing a callback. When a callback is passed, no Promise is returned
+	spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function(err, data) {
+	  if (err) console.error(err);
+	  else console.log('Artist albums', data);
+	});
 }
 
-function getAllTrackIdsForQueryTest() {
-	const query1 = '';
-	const result1 = getAllTrackIdsForQuery(query1);
-	print(result1);
-	
-	const query2 = '1l234hlahdfa784rklasf134';
-	const result2 = getAllTrackIdsForQuery(query2);
-	print(result2);
-	
-	const query3 = 'love';
-	const result3 = getAllTrackIdsForQuery(query3);
-	print(result3);
-	
-	const query4 = 'love is';
-	const result4 = getAllTrackIdsForQuery(query4);
-	print(result4);
-}
 
-getAllTrackIdsForQueryTest();
+/// Test Functions 
+// function getAllTrackIdsForQueryTest1() {
+// 	var query1 = '';
+// 	var result1 = getAllTrackIdsForQuery(query1);
+// 	print(result1);
+// }
+// 
+// function getAllTrackIdsForQueryTest2() {	
+// 	var query2 = '1l234hlahdfa784rklasf134';
+// 	var result2 = getAllTrackIdsForQuery(query2);
+// 	print(result2);
+// }
+// 
+// function getAllTrackIdsForQueryTest3() {	
+// 	var query3 = 'love';
+// 	var result3 = getAllTrackIdsForQuery(query3);
+// 	print(result3);
+// }
+// 
+// function getAllTrackIdsForQueryTest4() {	
+// 	var query4 = 'love is';
+// 	var result4 = getAllTrackIdsForQuery(query4);
+// 	print(result4);
+// }
